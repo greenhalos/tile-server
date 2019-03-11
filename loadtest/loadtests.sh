f06c2b6c5775dc21bb24f20a2ffe088e6eacd981
Ben Antony <greenhalos@gmail.com>
2019-03-12 00:38:01 +0100

    Add layer tests to loadtests

--- loadtest/loadtests.sh
+++ loadtest/loadtests.sh
@@ -4,8 +4,14 @@ HOST=$1
 Z=$2
 X=$3
 Y=$4
+LAYER_NAME=$5
 
-URL="https://$HOST/tiles/$Z/$X/$Y.pbf?cache=False"
+if [ -z $LAYER_NAME ]; then
+  URL="https://$HOST/tiles/$Z/$X/$Y.pbf?cache=False"
+else
+  URL="https://$HOST/tiles/$LAYER_NAME/$Z/$X/$Y.pbf?cache=False"
+fi;
+
 
 CURL_RESULT=$(curl -w "@curl-format.txt" -o /dev/null -s $URL)
-echo "$Z;$X;$Y;$CURL_RESULT;$URL"
+echo "$Z;$X;$Y;$CURL_RESULT;$LAYER_NAME;$URL"
--- loadtest/runAll.sh
+++ loadtest/runAll.sh
@@ -2,11 +2,21 @@
 
 HOST=$1
 
-echo "z;x;y;time_total;size_download;url"
+echo "z;x;y;time_total;size_download;layer;url"
 
-./loadtests.sh $HOST 15 17150 11250
-./loadtests.sh $HOST 13 4288 2812
-./loadtests.sh $HOST 13 4289 2812
-./loadtests.sh $HOST 10 534 351
-./loadtests.sh $HOST 9 267 176
-./loadtests.sh $HOST 3 4 2
+declare -a LAYERS=($(cat ../tileServer/app/zoomTables.yml | grep -e ":$" | cut -d ':' -f1))
+
+function executeLoadtest() {
+  ./loadtests.sh $HOST $1 $2 $3
+  for LAYER in "${LAYERS[@]}"
+  do
+     ./loadtests.sh $HOST $1 $2 $3 $LAYER
+  done
+}
+
+executeLoadtest 15 17150 11250
+executeLoadtest 13 4288 2812
+executeLoadtest 13 4289 2812
+executeLoadtest 10 534 351
+executeLoadtest 9 267 176
+executeLoadtest 3 4 2
